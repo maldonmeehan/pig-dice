46e4cb6bad2802e7996667d8e665b7e33d7ede21
Maldon Meehan and Sam Park <sam@>
2016-06-15 16:22:14 -0700

    working totalScoreGenerator

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 function Player(isCurrentPlayer) {
   this.totalScore = 0;
   this.turnScore = 0;
-  this.rollScore = 0;
+  this.rollScore = [];
   this.activePlayer = isCurrentPlayer;
 }
 
@@ -12,19 +12,19 @@ function randomDieRoller() {
 }
 
 Player.prototype.turnScoreGenerator = function(dieRoll) {
-  this.turnScore+= this.dieRoll;
-  var turnScore;
-  if (this.dieRoll === 1) {
+  //this.turnScore += this.dieRoll;
+  // var turnScore;
+  if (dieRoll === 1) {
     this.turnScore = 0;
   } else {
-    this.turnScore += this.dieRoll;
+    this.turnScore += dieRoll;
   }
   return this.turnScore;
 }
 
 Player.prototype.totalScoreGenerator = function(turnScore) {
-  totalScore = totalScore + turnScore;
-  return totalScore;
+  this.totalScore = this.totalScore + this.turnScore;
+  return this.totalScore;
 }
 
 // switch players with true false (1 or hold)
@@ -33,23 +33,31 @@ Player.prototype.totalScoreGenerator = function(turnScore) {
 // Frontend logic
 $(document).ready(function() {
   var player1 = new Player(true);
+  var player2 = new Player(false);
 
   $("#roll").click(function(event){
     event.preventDefault();
-    debugger;
-    var dieRoll = randomDieRoller()
+    // debugger;
+    var dieRoll = randomDieRoller();
     $("#die1").text(dieRoll);
 
-    var turnScore = totalScoreGenerator(dieRoll);
-    $("#player1").text(turnScore);
+    var turnScore = player1.turnScoreGenerator(dieRoll);
+    $("#player1").text(player1.turnScore);
     $("h4#player1").append("<h4>" + "Player 1: " + totalScore + "</h4>");
+
+    // var turnScore = player2.totalScoreGenerator(dieRoll);
+    // $("#player2").text(turnScore);
+    // $("h4#player2").append("<h4>" + "Player 2: " + totalScore + "</h4>");
   })
 
   $("#hold").click(function(event){
     event.preventDefault();
 
-    var totalScore = totalScoreGenerator(turnScore);
+    var totalScore = player1.totalScoreGenerator(turnScore);
     $("#player1").text(totalScore);
+
+    var totalScore = player2.totalScoreGenerator(turnScore);
+    $("#player2").text(totalScore);
   })
 });
 
