586f04d0e4ff9d5e44237b6fddaba2da8bcdd84e
Maldon Meehan and Sam Park <sam@>
2016-06-15 15:36:02 -0700

    add contractor and prototypes that have yet to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,57 @@
 // backend logic
-var player1Array = [];
-var player2Array = [];
+function Player(isCurrentPlayer) {
+  this.totalScore = 0;
+  this.turnScore = 0;
+  this.rollScore = 0;
+  this.activePlayer = isCurrentPlayer;
+}
 
-function rollDice() {
-  var die1 = document.getElementbyId("die1");
-  var die2 = document.getElementbyId("die2");
-  var status = document.getElementbyId("status");
-  var d1 = Math.floor(Math.random() * 6) + 1;
-  var d2 = Math.floor(Math.random() * 6) + 1;
-  var diceTotal = d1 + d2;
-    return diceTotal;
-    if(d1 === 1 || d2 === 1){
-      // switch players
-    };
-// randomizer
-// If player rolls a 2-6
-// If player rolls a 1
-// Add current roll score
+function randomDieRoller() {
+  var randomDieRoll = Math.floor((Math.random() * 6) +1);
+  return randomDieRoll;
+}
+
+Player.prototype.turnScoreGenerator = function(dieRoll) {
+  this.turnScore+= this.dieRoll;
+  var turnScore;
+  if (this.dieRoll === 1) {
+    this.turnScore = 0;
+  } else {
+    this.turnScore += this.dieRoll;
+  }
+  return this.turnScore;
+}
+
+Player.prototype.totalScoreGenerator = function(turnScore) {
+  totalScore = totalScore + turnScore;
+  return totalScore;
+}
+
+// switch players with true false (1 or hold)
+// if total score is 100, than game win
 
 // Frontend logic
+$(document).ready(function() {
+  var player1 = new Player(true);
 
-$("button#roll").click(function()
-  
-});
+  $("#roll").click(function(event){
+    event.preventDefault();
+    debugger;
+    var dieRoll = randomDieRoller()
+    $("#die1").text(dieRoll);
 
-$("button#hold").click(function()
+    var turnScore = totalScoreGenerator(dieRoll);
+    $("#player1").text(turnScore);
+    $("h4#player1").append("<h4>" + "Player 1: " + totalScore + "</h4>");
+  })
 
+  $("#hold").click(function(event){
+    event.preventDefault();
+
+    var totalScore = totalScoreGenerator(turnScore);
+    $("#player1").text(totalScore);
+  })
 });
 
 // status.innerHTML = "You rolled "+diceTotal+".";
 // status.innerHTML += " You rolled a 1, you lose your points this round and your turn!";
-// };
