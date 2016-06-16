745254dd191c2690c7f5cce6a4452ad3ad293925
Maldon Meehan and Sam Park <sam@>
2016-06-16 14:50:52 -0700

    will allow rollscore and totalscore to display for both users

--- js/scripts.js
+++ js/scripts.js
@@ -1,88 +1,98 @@
 // backend logic
-function Player(isCurrentPlayer) {
-  this.totalScore = 0;
-  this.turnScore = 0;
-  this.rollScore = [];
-  this.activePlayer = true;
+var player1 = new Player(0, 0, 0, true);
+var player2 = new Player(0, 0, 0, false);
+
+function Player(totalScore, roundScore, rollScore, activePlayer) {
+  this.totalScore = totalScore; //total amt of points over rounds
+  this.roundScore = roundScore;  //score per turn b4 switching
+  this.rollScore = rollScore; //current value of die
+  this.activePlayer = activePlayer;
 }
 
-function randomDieRoller() {
-  var randomDieRoll = Math.floor((Math.random() * 6) +1);
-  return randomDieRoll;
+Player.prototype.randomDieRoller = function() { //makes random number.
+  this.rollScore = Math.floor((Math.random() * 6) +1);
 }
 
-Player.prototype.turnScoreGenerator = function(dieRoll) {
-  if (dieRoll === 1) {
-    // var totalScore
-    this.turnScore = 0;
-    this.switch();
+Player.prototype.roundScoreGenerator = function() {
+  if (this.rollScore === 1) {
+    this.roundScore = 0;
   } else {
-    this.turnScore += dieRoll;
-  }
-  return this.turnScore;
-}
-
-Player.prototype.totalScoreGenerator = function(turnScore) {
-  this.totalScore = this.totalScore + this.turnScore;
-  return this.totalScore;
-}
-
-Player.prototype.activePlayer = function (activePlayer){
-  if (dieRoll === 1)
-  this.switch();
-  else {
-    this.totalScore += this.turnScore;
+    this.roundScore += this.rollScore;
   }
 }
 
-Player.prototype.switch = function()  {
-  if (this === player1) {
+Player.prototype.totalScoreGenerator = function() {
+  this.totalScore += this.roundScore;
+  // this.turnScore = 0;
+  // this.totalScoreArray.push(this.roundScore);
+  // for (var i = 0; i < totalScoreArray.length; i ++){
+  //   this.totalScoreArray = roundScore + totalScoreArray[i];
+}
+
+//switch
+Player.prototype.activePlayer = function() {
+  if (player1.activePlayer === true) {
     player1.activePlayer = false;
     player2.activePlayer = true;
-  } else if (this === player2) {
-    player2.activePlayer = false;
+  } else {
     player1.activePlayer = true;
+    player2.activePlayer = false;
   }
 }
 
-// switch players with true false (1 or hold)
+Player.prototype.playerSwitch = function() {
+  if (this.activePlayer === true) {//may have to revisit?
+    this.activePlayer = false;
+  } else {
+    this.activePlayer == true;
+  }
+}
 // if total score is 100, than game win
 
 // Frontend logic
 $(document).ready(function() {
-  var player1 = new Player(0, 0, 0, true);
-  var player2 = new Player(0, 0, 0, false);
 
-  $("#roll").click(function(event){
-    event.preventDefault();
-    var dieRoll = randomDieRoller();
-    $("#die1").text(dieRoll);
-
-    var turnScore = player1.turnScoreGenerator(dieRoll);
-    $("#player1turn").text(player1.turnScore);
-    // $("h4#player1").append("<h4>" + "Player 1: " + totalScore + "</h4>");
-
-    var turnScore = player1.totalScoreGenerator(dieRoll);
-    $("#player1").text(player1.totalScore);
-
-    var turnScore = player2.totalScoreGenerator(dieRoll);
-    $("#player2turn").text(player2.totalScore);
-
-    var turnScore = player2.turnScoreGenerator(dieRoll);
-    $("#player2").text(player2.turnScore);
-    // $("h4#player2").append("<h4>" + "Player 2: " + totalScore + "</h4>");
-  })
-
-  $("#hold").click(function(event){
+  $("#roll").click(function(event) {
     event.preventDefault();
 
-    var totalScore = player1.totalScoreGenerator(turnScore);
-    $("#player1").text(totalScore);
+    if (player1.activePlayer === true) {
+      player1.randomDieRoller();
+      player1.roundScoreGenerator(player1.rollScore);
+      $("#die1").text(player1.rollScore);
+      $("#player1round").text(player1.roundScore);
 
-    var totalScore = player2.totalScoreGenerator(turnScore);
-    $("#player2").text(totalScore);
-  })
-});
+    } else if (player1.activePlayer === false)  {
+      player2.randomDieRoller();
+      player2.roundScoreGenerator(player2.rollScore);
+      $("die1").text(player2.rollScore);
+      $("#player2round").text(player2.roundScore);
+    }
+  });
+
+  $("#hold").click(function(event) {
+    event.preventDefault();
+
+    if (player1.activePlayer === true) {
+      player1.totalScoreGenerator();
+      $("#player1total").text(player1.totalScore);
+      $("#die1").text(player1.rollScore);
+      player1.playerSwitch();
+
+
+    } else if (player2.activePlayer === false) {
+      player2.totalScoreGenerator();
+      $("#player2total").text(player2.totalScore);
+      $("#die1").text(player2.rollScore);
+      player2.playerSwitch();
+    }
+    });
+  });
+    // var totalScore = player1.totalScoreGenerator(roundScore);
+    // $("#player1").text(totalScore);
+    //
+    // var totalScore = player2.totalScoreGenerator(roundScore);
+    // $("#player2").text(totalScore);
+
 
 // status.innerHTML = "You rolled "+diceTotal+".";
 // status.innerHTML += " You rolled a 1, you lose your points this round and your turn!";
