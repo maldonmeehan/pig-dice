3e83014ad4418154cd46cbdf13055b303cb75d7d
Maldon Meehan and Sam Park <maldon@>
2016-06-16 09:49:15 -0700

    provide two different outputs but still issue with separation of players

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,7 @@ Player.prototype.turnScoreGenerator = function(dieRoll) {
   if (dieRoll === 1) {
     // var totalScore
     this.turnScore = 0;
+    this.switch();
   } else {
     this.turnScore += dieRoll;
   }
@@ -34,12 +35,21 @@ Player.prototype.activePlayer = function (activePlayer){
   }
 }
 
+Player.prototype.switch = function()  {
+  if (this === player1) {
+    player1.activePlayer = false;
+    player2.activePlayer = true;
+  } else if (this === player2) {
+    player2.activePlayer = false;
+    player1.activePlayer = true;
+  }
+}
+
 // switch players with true false (1 or hold)
 // if total score is 100, than game win
 
 // Frontend logic
 $(document).ready(function() {
-  debugger;
   var player1 = new Player(0, 0, 0, true);
   var player2 = new Player(0, 0, 0, false);
 
@@ -49,11 +59,17 @@ $(document).ready(function() {
     $("#die1").text(dieRoll);
 
     var turnScore = player1.turnScoreGenerator(dieRoll);
-    $("#player1").text(player1.turnScore);
+    $("#player1turn").text(player1.turnScore);
     // $("h4#player1").append("<h4>" + "Player 1: " + totalScore + "</h4>");
 
+    var turnScore = player1.totalScoreGenerator(dieRoll);
+    $("#player1").text(player1.totalScore);
+
     var turnScore = player2.totalScoreGenerator(dieRoll);
-    $("#player2").text(turnScore);
+    $("#player2turn").text(player2.totalScore);
+
+    var turnScore = player2.turnScoreGenerator(dieRoll);
+    $("#player2").text(player2.turnScore);
     // $("h4#player2").append("<h4>" + "Player 2: " + totalScore + "</h4>");
   })
 
